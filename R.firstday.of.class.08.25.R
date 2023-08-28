bfa7800cf1d07940ce5ae58e8e709845de447353
unknown <owner@DESKTOP-I7Q6HF5>
2023-08-28 18:49:17 -0400

    commit example

---
+++ R.firstday.of.class.08.25.R
@@ -0,0 +1,12 @@
+2+2
+#use hashtags to comment and explain code
+#this is just a text file.
+#this is for writing text that is sent to R and run
+#must use CTRL enter to run and to have  record of code I have written
+
+#first assignment is to write responses to hw articles using quarto. Practice using markdown. 
+#show how to format a document--useful to show explanation with code for research purposes 
+#use github to share files to professor
+#set format to gfm--github flavored markdown
+#send to github to get html file
+#for hw, include gfm and qmd file
